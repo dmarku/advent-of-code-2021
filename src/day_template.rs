ee4309ffc88ed0645730b1ed7f9b00a9891decec
Markus Dittmann <dmarku@posteo.de>
2021-12-16 00:19:41 +0100

    add day module template

---
+++ src/day_template.rs
@@ -0,0 +1,47 @@
+mod task_01 {
+    // implementation goes here
+
+    #[cfg(test)]
+    mod test {
+        #[test]
+        fn example() {
+        }
+
+        #[test]
+        fn task() -> Result<(), Box<dyn Error>> {
+            // let solution = /* ... */
+            
+            // output result for input to AoC website
+            // println!("result: {}", result);
+            
+            // once the solution is correct, assert it in test results
+            // assert_eq!(solution, /* expected result */);
+
+            Ok(())
+        }
+    }
+}
+
+mod task_02 {
+    // implementation goes here
+    
+    #[cfg(test)]
+    mod test {
+        #[test]
+        fn example() {
+        }
+
+        #[test]
+        fn task() -> Result<(), Box<dyn Error>> {
+            // let solution = /* ... */
+            
+            // output result for input to AoC website
+            // println!("result: {}", result);
+            
+            // once the solution is correct, assert it in test results
+            // assert_eq!(solution, /* expected result */);
+
+            Ok(())
+        }
+    }
+}
